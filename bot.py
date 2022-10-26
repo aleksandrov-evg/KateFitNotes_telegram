f90bd93b493cd8b5c96e0f40843c68886b5dd0ab
Lenovo <alexandrovevg@gmail.com>
2022-10-26 15:38:21 +0300

    add requirements.txt file add some code bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,23 @@
 import configparser
+import telebot
 
 config = configparser.ConfigParser()
 config.read("config.ini")
+token = config["main"]["TOKEN"]
 
-print(config["main"]["TOKEN"])
+bot = telebot.TeleBot(token)
+
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == "Привет":
+        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+    elif message.text == "/help":
+        bot.send_message(message.from_user.id, "Напиши привет")
+    else:
+        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+
+
+#bot.polling(none_stop=True, interval=0)
+
 
